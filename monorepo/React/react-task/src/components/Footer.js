c7f7fb0fe53724d15e0ec79bbd11a707fc89f71b
Melisa-Mullahasani <mullahasanimelisa@gmail.com>
2023-10-05 17:54:27 +0200

    Detail changes

--- monorepo/React/react-task/src/components/Footer.js
+++ monorepo/React/react-task/src/components/Footer.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import PabLogo from '../images/pab-logo.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      Powered By 
-      <img src={PabLogo} className="pab-logo" alt='logo'></img> 
-      Pabau
+      Powered By IT Company
     </footer>
   )
 }
